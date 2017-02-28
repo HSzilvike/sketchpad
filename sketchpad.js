d73631cacd85b729d879e2eed9b0e9c705c7a89f
Szilvia Harmat <szh.nz@yahoo.com>
2017-02-28 14:44:42 +1300

    Add random color option

--- sketchpad.js
+++ sketchpad.js
@@ -9,26 +9,58 @@ $('.squarediv').on('mouseenter',function () {
 
 } );
 
-$('#usergrid').on('click', function() {
+	$('#usergrid').on('click', function() {
 
-	var newsize = prompt("How many squares per side would you like?", "16");
+		var newsize = prompt("How many squares per side would you like?", "16");
 
-	$(".squarediv").remove();
+		$(".squarediv").remove();
 
-	var newwidth = 960/newsize;
+		var newwidth = 960/newsize;
 
-	gridcreation(newsize);
+		gridcreation(newsize);
 
-	$(".squarediv").css( {"width":newwidth, "height":newwidth });
+		$(".squarediv").css( {"width":newwidth, "height":newwidth });
 
-	$('.squarediv').on('mouseenter',function () {
+		$('.squarediv').on('mouseenter',function () {
 
-	$(this).addClass("highlight")
-} );
+		$(this).addClass("highlight") } );
+
+	});
+
+
+	$('#pencil').on('click', function() {
+
+		$('.squarediv').removeClass("highlight");
+		$('.squarediv').css({"background-color":""});
+
+			$('.squarediv').on('mouseenter',function () {
+
+			$(this).css({"background-color":"grey"});
+
+
+	});
+
+	});
+
+
+	$('#color').on('click', function() {
+
+			$('.squarediv').removeClass("highlight");
+			$('.squarediv').css({"background-color":""});
+
+			$('.squarediv').on('mouseenter',function () {
+
+			$(this).css({"background-color": randomcolor()});
+
+			});
+
+
+	});
 
 
 
-});
+
+
 
 
 });
@@ -47,4 +79,15 @@ var gridcreation = function(size) {
 $('#container').append('<div class="squarediv"></div>');
 
 };
-}
+};
+
+
+var randomcolor = function() {
+
+var rgb1 = Math.floor(Math.random() * 255);
+var rgb2 = Math.floor(Math.random() * 255);
+var rgb3 = Math.floor(Math.random() * 255);
+
+return "rgb("+rgb1+ ","+ rgb2+","+ rgb3 +")";
+
+};
